0df249236b28cb7bc19d6a2a364ac5a790876d3c
moshihao <1786600649@qq.com>
2025-04-08 19:48:07 +0800

    添加路由

--- src/router.ts
+++ src/router.ts
@@ -1,13 +1,15 @@
 import { createWebHashHistory, createRouter } from 'vue-router'
 import Home from './components/Home.vue'
 import Doc from './components/Doc.vue'
+import QRVue from './components/QR.vue'
 
 const history = createWebHashHistory()
 export const router = createRouter({
   history:createWebHashHistory(),
   routes: [
     { path: '/', component: Home },
-    { path: '/doc', component: Doc }
+    { path: '/doc', component: Doc },
+    { path: '/qr', component: QRVue}
   ]
 })
 router.afterEach(() => {
